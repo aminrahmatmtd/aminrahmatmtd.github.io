42564ef528a99f571af543a342146f86183035c2
unknown <aminrahmatmtd@gmail.com>
2024-01-08 12:44:35 +0700

    first commit

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="c-footer">
+    <div>Gospend © {{ date('Y') }} || Developed by <strong><a target="_blank" href="https://www.vaganza.co.id/">Jokiku Solusi Topup </a></strong></div>
+
+    <div class="mfs-auto d-md-down-none">Version <strong class="text-danger">1.1.0</strong></div>
+</footer>
